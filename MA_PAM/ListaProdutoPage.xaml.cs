93993ae4a1d5bc265608e96162dff5e831bf68f0
Kayo Henrique <230232@LTI5401>
2025-04-22 17:05:21 -0300

    Feito em aula

--- MA_PAM/ListaProdutoPage.xaml.cs
+++ MA_PAM/ListaProdutoPage.xaml.cs
@@ -13,4 +13,18 @@ public partial class ListaProdutoPage : ContentPage
     {
         Navigation.PushAsync(new ProdutoPage() { BindingContext = ((ViewCell)sender).BindingContext});
     }
+
+    private void FiltrarPorCategoria(object sender, EventArgs e) 
+    {
+        string categoriaSelecionada = filtroCategoriaPicker.SelectedItem?.ToString() ?? "Todos";
+        if (categoriaSelecionada == "Todas")
+        {
+            produtoListView.ItemsSource = MainPage.Produto.OrderBy(p => p.Acessórios).ToList();
+        }
+        else 
+        { 
+        
+        }
+
+    }
 }
